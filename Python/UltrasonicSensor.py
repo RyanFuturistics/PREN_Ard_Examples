bb6670ffcaca37cbee651c0a1674bd1f795dc545
tsn <thomas.stadelmann.01@stud.hslu.ch>
2017-04-27 01:00:37 +0200

    Restructering

--- Python/UltrasonicSensor.py
+++ Python/UltrasonicSensor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import threading
-import time, random
+import time
 import logging
 import RPi.GPIO as GPIO
-import math
+
 
 logging.basicConfig(level=logging.CRITICAL)
 
@@ -12,21 +12,18 @@ log = logging.getLogger(__name__)
 GPIO.setmode(GPIO.BOARD)
 
 
-class Ultrasonic_Sensor:
-
+class UltrasonicSensor:
     def __init__(self, trigger_pin, echo_pin, n=10):
         self.trigger_pin = trigger_pin
         self.echo_pin = echo_pin
         self.n = n
-        self.counter=0
+        self.counter = 0
 
         self.values = [0] * n
         self.no_echo = False
 
-        GPIO.setup(echo_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+        GPIO.setup(echo_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(trigger_pin, GPIO.OUT)
-        self.write_pin(0)
-
         self.write_pin(0)
 
     def read_pin(self):
@@ -41,33 +38,33 @@ class Ultrasonic_Sensor:
         self.write_pin(0)
 
     def convert_to_cm(self, delay_time):
-        return (delay_time * 1E6 ) / 58
+        return (delay_time * 1E6) / 58
 
     def mean(self):
         return float(sum(self.values)) / max(len(self.values), 1)
 
     def get_last_value(self):
-        return self.values[self.n-1]
+        return self.values[self.n - 1]
 
     def __get_value__(self):
         log.debug("Trigger")
         self.trigger()
 
         start = time.time()
-        c = GPIO.wait_for_edge(self.echo_pin, GPIO.RISING, timeout=60)
+        c2 = GPIO.wait_for_edge(self.echo_pin, GPIO.RISING, timeout=60)
         pulse = time.time()
         c2 = GPIO.wait_for_edge(self.echo_pin, GPIO.FALLING, timeout=30)
 
-        pulse_duration = time.time()-pulse
+        pulse_duration = time.time() - pulse
 
         if c2 is None:
-             log.debug("No Echo")
-             self.no_echo = True
+            log.debug("No Echo")
+            self.no_echo = True
 
         else:
             self.no_echo = True
             distance = self.convert_to_cm(pulse_duration)
-            log.info( "echo " + str(distance) + "cm")
+            log.info("echo " + str(distance) + "cm")
             measurement_time = time.time() - start
 
             self.counter += 1
@@ -79,13 +76,26 @@ class Ultrasonic_Sensor:
                 time.sleep(.06 - measurement_time)
             return distance
 
-class Sensor_Array:
-    def __init__(self):
-        self.sensors = []
-        self.running = True;
 
-    def addSensor(self, Sensor):
-        self.sensors.append(Sensor)
+class SensorArray:
+    FL = 0
+    FR = 1
+    LF = 1
+    RF = 2
+    LR = 3
+    RR = 4
+
+    trigger_pins = [1, 2, 3, 4, 5]
+    echo_pins = [1, 2, 3, 4, 5]
+
+    def __init__(self, n=6):
+        self.sensors = [None] * n
+        self.running = True
+        self.init = 0  # Keeps count when initalising Sensors
+
+    def add_sensor(self):
+        self.sensors[self.init] = UltrasonicSensor(self.trigger_pins[self.init], self.echo_pins[self.init])
+        self.init += 1
 
     def start_measurement_loop(self):
         self.running = True
@@ -97,18 +107,15 @@ class Sensor_Array:
         self.running = False
 
 
-
-
-
 if __name__ == "__main__":
-    #logs sensor data on disk
+    # logs sensor data on disk
 
-    s1 = Ultrasonic_Sensor(3, 11)
+    s1 = UltrasonicSensor(3, 11)
 
-    a = Sensor_Array()
-    a.addSensor(s1)
+    a = SensorArray()
+    a.add_sensor(s1)
 
-    t1 = threading.Thread(target= a.start_measurement_loop)
+    t1 = threading.Thread(target=a.start_measurement_loop)
     t1.start()
     time.sleep(1)
 
@@ -116,12 +123,10 @@ if __name__ == "__main__":
         with open("Sensor.log", "a") as f:
             for sensor in a.sensors:
                 f.write("{:.2f};".format(sensor.mean()))
-            
+
             f.write("\n")
             time.sleep(.06 * len(a.sensors))
-
 
     a.stop_measurement_loop()
     t1.join()
     log.info("stoped gracefully")
-
